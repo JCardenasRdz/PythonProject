2666050580f06475a41134bc8f3a1faeb8974ab4
virginiayung <yungvirginia03@gmail.com>
2014-07-19 00:02:22 -0700

    Changes in term_sentiment.py

--- UW_Data_Science_Coursera/assignment1/term_sentiment.py
+++ UW_Data_Science_Coursera/assignment1/term_sentiment.py
@@ -4,6 +4,7 @@ import string
 from collections import defaultdict
 
 
+
 """
 Part 3:
 a script that computes the sentiment for the terms that do not appear in the file 
@@ -42,16 +43,16 @@ def cleanup(raw_text):
 
 
 def sent_score(w):
-	sent_file = open(sys.argv[1])
-	scores = {} # initialize an empty dictionary
-	for line in sent_file:
-  		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-  		scores[term] = int(score)  # Convert the score to an integer.
+  	
+  	for key, val in scores.items():
 
-
-  		if w == term:
+  		if w == key:
+  			
+  			sent_dictionary[w] = val
+  			
   			global sum_score
-  			sum_score += scores[w]
+  			sum_score += sent_dictionary[w]
+
 			#print w, scores[w], 'total:',sum_score
 
 			# if scores[w] > 0:
@@ -64,95 +65,95 @@ def sent_score(w):
 			# 	global neg_sum
 			# 	neg_sum += scores[w]
 			# 	#print neg_sum
-
 			sent.append(w)
+	
+			
   		else:
   			
-  			scores[w] =0
 
-  		
+  			sum_score = sum_score
+
+  			#non_sent.append(w)
   			
 
-	# Print every (term, score) pair in the dictionary
-	#print scores.items() 
 
-
-	
-	
 
 if __name__ == '__main__':
 
 	#print "Running sentiment analysis on tweets..."
 	main()
 
+	# Converting sentiment file into dictionary
+	sent_file = open(sys.argv[1])
+	scores = {} # initialize an empty dictionary
+	
+	for line in sent_file:
+  		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+  		scores[term] = int(score)  # Convert the score to an integer.
+  
+
 	#The set of punctuation in the string module
 	punctuations = set(string.punctuation)
 	
-    #tweets =[rec['text'] for rec in tweets_full if 'text' in rec]
+  
 	
   	tweets_full= [json.loads(line) for line in open(sys.argv[2])]
-	#print tweets_full[0]
+
 	tweets_all = [rec for rec in tweets_full if 'text' and 'lang' in rec]
 	engtweets = eng_tweets(tweets_all, "lang", "en")
 	#tweets =[eng['lang'] for eng in tweets if 'lang' in eng] #checking languages of all english tweets.
 
-	#print 'Number of English Tweets: ', len(engtweets)
-
+	
 	tweets = [t['text'] for t in engtweets]
 
 	
-	
+	tweets = tweets [2:5]
+	sent_dictionary = {}
+
 	# Initialize new dictionary for non_sent terms. key: terms. val: sent_score
 	non_sent_dict = {}
 
 	# Initialize new dictionary for non_sent terms. key: terms. val: counter
 	non_sent_dub_term = {}
 
+	sum_non_sent_dub_term ={}
 
-	#For each tweet, print index, content of tweet, parse out content, calculate sent score
+	# non_sent = []
+	
+	sent = []
+
+	non_sent_score = 0
+	
+
+	# For each tweet, print index, content of tweet, parse out content, calculate sent score
 	for idx_t, val_t in enumerate(tweets):
 		unicode_string = tweets[idx_t]
 		#print 'upper:', encoded_string
 		encoded_string = unicode_string.encode('utf-8')
 		#print 'lower:', encoded_string
 		encoded_string = encoded_string.lower()
-
 		
-		encoded_string = encoded_string.replace('!', "")
-		encoded_string = encoded_string.replace('?', "")
-		encoded_string = encoded_string.replace('.', "")
+	
 		encoded_string = cleanup(encoded_string)
 
 		d = defaultdict(list)
 		splt_tweet = encoded_string.split()
 
-		# Creating new dict to store non-sentiment-carrying terms
-
 		
-		non_sent = []
-		sent = []
-		# Initialize sum to be 0 for every tweet.
-		#print idx_t, encoded_string
-		pos_sum = 0
-		neg_sum = 0
 		sum_score = 0
-		
+	
 		for idx_w, val_w in enumerate(splt_tweet):
 			words = splt_tweet[idx_w]
-
-			# Matching words to Sentiment Scores in AFINN-111.txt
 			sent_score(words)
+	
+		#print list(set(splt_tweet))
+		sent = list(set(sent))
+		
+		non_sent_b= [elem for elem in splt_tweet if elem not in (sent)]
+		non_sent_b = list(set(non_sent_b))
 
-		#print 'pos_sum',pos_sum
-		#print 'neg_sum', neg_sum
-
-
-		non_sent_a= [elem for elem in splt_tweet if elem not in (sent)] 
-
-		# Removing Duplicates (Unordered distinct items)
-		non_sent_b= list(set(non_sent_a))
-		non_sent_score = sum_score
-
+		non_sent_score = (float(sum_score))
+		
 
 		for val_n in non_sent_b:
 
@@ -160,20 +161,29 @@ if __name__ == '__main__':
 			if val_n not in non_sent_dict: 
 				non_sent_dict[val_n] = non_sent_score
 				non_sent_dub_term[val_n] = 1
+				sum_non_sent_dub_term[val_n]= non_sent_score
 			
 			else:
 				non_sent_dub_term[val_n] += 1
-				
-				non_sent_dict[val_n] = (non_sent_score+ non_sent_dict[val_n])/non_sent_dub_term[val_n]
-				#print val_n, non_sent_dub_term
 
-	#print 'non_sent_dict:',non_sent_dict
+				sum_non_sent_dub_term[val_n] += non_sent_score
+
+				
+				non_sent_dict[val_n] =float(sum_non_sent_dub_term[val_n])/float(non_sent_dub_term[val_n])
+		
+			#print non_sent_score, non_sent_dict, non_sent_dub_term
+
+	
 	for key, val in non_sent_dict.items():
 		if len(key) == 0:
 			continue
-		print key, int(val)
-
+		print key, float(val)
+		print type(key), type(val)
 	
+	
+
+
+
 
 
 		
